228852a5f4ca93af52e117e13f996d969da77990
Patrick Furtak <patrick@patrickfurtak.com>
2020-05-08 22:58:16 -0400

    created floating button for adding a new song, brought in react router to redirect back to songs on successful song creation

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link, hashHistory } from 'react-router';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
 
@@ -11,16 +12,20 @@ class SongCreate extends Component {
   onSubmit(event) {
     event.preventDefault();
 
-    this.props.mutate({
-      variables: {
-        title: this.state.title,
-      },
-    });
+    this.props
+      .mutate({
+        variables: {
+          title: this.state.title,
+        },
+      })
+      .then(() => hashHistory.push('/'))
+      .catch((error) => console.error(error));
   }
 
   render() {
     return (
       <div>
+        <Link to='/'>Back</Link>
         <h3>Create a New Song</h3>
         <form onSubmit={this.onSubmit.bind(this)}>
           <label>Song Title:</label>
